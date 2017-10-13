c307a39fef3c00d5c8f92a2ae7940bc176eda630
cc <cc@example.com>
2017-10-13 11:03:43 +0800

    add index view & detail view

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.PostView.as_view(), name='detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Post
+
+
+class IndexView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/index.html'
+
+
+class PostView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/detail.html'
