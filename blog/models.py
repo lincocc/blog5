d9e9a10715a4cd07235d9c048bbd037ab37a5f3c
cc <cc@example.com>
2017-10-13 10:52:09 +0800

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_time = models.DateTimeField(default=timezone.now)
+    modified_time = models.DateTimeField(auto_now=True)
+    excerpt = models.CharField(max_length=200, blank=True)
+    views = models.PositiveIntegerField(default=0)
+    author = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_time']
